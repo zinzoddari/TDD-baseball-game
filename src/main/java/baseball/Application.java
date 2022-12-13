39359b04f6d013a6aafcd40113fc5fd13a486cbc
zinzoddari <zinzoddari@gmail.com>
2022-12-14 00:12:16 +0900

    feat: 메인 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import domain.Balls;
+import domain.Result;
+import nextstep.utils.Console;
+import nextstep.utils.Randoms;
+
 public class Application {
+    private static Result result;
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        result = new Result();
+        Balls computer = new Balls("" + Randoms.pickNumberInRange(1, 9) + Randoms.pickNumberInRange(1, 9) + Randoms.pickNumberInRange(1, 9));
+        while (!result.isGameEnd()) {
+            result = computer.play(new Balls(Console.readLine()));
+            result.result();
+
+            if(result.isGameEnd()) {
+                System.out.println("게임 끝");
+                if (Console.readLine().equals("1")) {
+                    result = new Result();
+                    computer = new Balls("" + Randoms.pickNumberInRange(1, 9) + Randoms.pickNumberInRange(1, 9) + Randoms.pickNumberInRange(1, 9));
+                }
+            }
+        }
     }
 }
